cafd3c5abe5c32f5feaaf0bf5ba4b6e8c474b325
Vagner Kuntz <vaagner.kuntz@gmail.com>
2021-08-06 10:51:54 -0300

    base do projeto com o template basico criado

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const mongo = process.env.MONGODB || 'mongodb://localhost/noticias'
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
+
+mongoose
+  .connect(mongo, { useMongoClient: true })
+  .then(() => {
+    app.listen(port, () => console.log('listening...'))
+  })
+  .catch(e => console.log(e))
