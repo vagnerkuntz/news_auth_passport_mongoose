5bb17b773da958e1a44b73e1b015d57cadd35513
Vagner Kuntz <vaagner.kuntz@gmail.com>
2021-08-06 12:42:51 -0300

    criando usuario padrao no banco

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
+const User = require('./models/user')
+
 const mongo = process.env.MONGODB || 'mongodb://localhost/noticias'
 
 app.set('views', path.join(__dirname, 'views'))
@@ -12,11 +14,27 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+const createInitialUSer = async() => {
+  const total = await User.countDocuments({ username: 'adminsis' })
+  if (total === 0) {
+    const user = new User({
+      username: 'adminsis',
+      password: '123'
+    })
+    await user.save()
+    console.log('user admin created')
+  } else {
+    console.log('user admin created skipped')
+  }
+}
+
 app.get('/', (req, res) => res.render('index'))
 
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    createInitialUSer()
     app.listen(port, () => console.log('listening...'))
   })
   .catch(e => console.log(e))
+
