75d929af6c232a25b40e2ea6dc38a12b8451d21c
Vagner Kuntz <vaagner.kuntz@gmail.com>
2021-08-06 13:26:58 -0300

    restringindo o acesso e fazendo o login

--- index.js
+++ index.js
@@ -3,16 +3,54 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
+
 mongoose.Promise = global.Promise
 
 const User = require('./models/user')
+const noticias = require('./routes/noticias')
+const restrito = require('./routes/restrito')
+const session = require('express-session')
 
 const mongo = process.env.MONGODB || 'mongodb://localhost/noticias'
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(session({
+  secret: 'sḰ3,2432-0Apwty',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+app.use('/restrito', (req, res, next) => {
+  if ('user' in req.session) {
+    return next();
+  }
+
+  res.redirect('/login')
+})
+
+app.use('/restrito', restrito)
+app.use('/noticias', noticias)
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({ username: req.body.username })
+  const isValid = await user.checkPassword(req.body.password)
+
+  if (isValid) {
+    req.session.user = user
+    res.redirect('/restrito/noticias')
+  } else {
+    res.redirect('/login')
+  }
+})
 
 const createInitialUSer = async() => {
   const total = await User.countDocuments({ username: 'adminsis' })
---
+++ routes/noticias.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+const Noticia = require('../models/noticia')
+
+
+router.get('/', (req, res) => {
+  res.send('noticias publicas')
+})
+
+module.exports = router
